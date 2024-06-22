4b0930aac6b0d5604a87dab023bfee5120a1c6ea
yyyy52 <yyxyuanyingxuan@outlook.com>
2024-06-22 18:09:08 +0800

    root base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,18 +3,18 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/yyyy52.github.io/",
 
   locales: {
     "/": {
       lang: "en-US",
       title: "Blog Demo",
-      description: "A blog demo for vuepress-theme-hope",
+      description: "A blog demo",
     },
     "/zh/": {
       lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
+      title: "博客",
+      description: "设置中的博客",
     },
   },
 
